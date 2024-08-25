5e914c42ba2c92ca9bcb3aca5ea8237f1ab68edf
Alan-Luc <alanluc9@gmail.com>
2024-08-25 16:18:47 -0400

    auth in progress

--- backend/handlers/userHandler.go
+++ backend/handlers/userHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"go/token"
 	"html"
 	"net/http"
 	"strings"
@@ -12,28 +13,28 @@ import (
 )
 
 func RegisterUser(ctx *gin.Context) {
-	var user models.User
+	var registerInput models.User
 	var err error
 
-	if err = ctx.ShouldBindJSON(&user); err != nil {
+	if err = ctx.ShouldBindJSON(&registerInput); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	err = PrepareUser(&user)
+	err = PrepareUser(&registerInput)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	err = CreateUser(&user)
+	err = CreateUser(&registerInput)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	} else {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "User registered successfully",
-			"user_id": user.ID,
+			"user_id": registerInput.ID,
 		})
 	}
 }
@@ -57,4 +58,50 @@ func PrepareUser(u *models.User) error {
 	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
 
 	return nil
+}
+
+func LoginUser(ctx *gin.Context) {
+	var loginInput models.User
+	var err error
+
+	if err = ctx.ShouldBindJSON(&loginInput); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error ": err.Error()})
+		return
+	}
+
+	jwt, err := VerifyUser(&loginInput.Username, &loginInput.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"Invalid credentials ": err.Error()})
+		return
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"token": jwt})
+	}
+
+}
+
+func VerifyUser(username, password *string) (string, error) {
+	var err error
+	var user models.User
+	// check if user in db
+	if err = database.DB.Model(&models.User{}).Find(&user, username).Error; err != nil {
+		return "", err
+	}
+
+	// verify password
+	var pw string = *password
+	err = VerifyPassword(user.Password, pw)
+	if err != nil {
+		return "", err
+	}
+
+	jwt, err := auth.GenerateToken(user.ID)
+	if err != nil {
+		return "", err
+	}
+
+	return jwt, nil
+}
+
+func VerifyPassword(hashedPw, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPw), []byte(password))
 }
